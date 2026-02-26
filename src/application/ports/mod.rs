5fdcbe357586bf850bbb859002262284ff01a626
Glauber Brennon <glauberbrennon@gmail.com>
2026-02-26 02:28:03 -0300

    refactor(application-ports): rename ports to have consistent naming

--- src/application/ports/mod.rs
+++ src/application/ports/mod.rs
@@ -1,8 +1,8 @@
 pub mod clock_port;
 pub mod create_medication_port;
-pub mod dose_record_repository;
-pub mod list_all_medications;
+pub mod dose_record_repository_port;
+pub mod list_all_medications_port;
 pub mod mark_dose_taken_port;
-pub mod medication_repository;
+pub mod medication_repository_port;
 pub mod notification_port;
 pub mod schedule_dose_port;
