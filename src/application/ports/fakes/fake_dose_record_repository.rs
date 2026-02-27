b1dd3d00f25e1bcb1372ab402b3cd85bb1419dea
Glauber Brennon <glauberbrennon@gmail.com>
2026-02-27 01:05:40 -0300

    refactor: use fakes for testing application services

---
+++ src/application/ports/fakes/fake_dose_record_repository.rs
@@ -0,0 +1,70 @@
+use std::sync::Mutex;
+
+use crate::application::errors::StorageError;
+use crate::application::ports::dose_record_repository_port::DoseRecordRepository;
+use crate::domain::{
+    entities::dose_record::DoseRecord,
+    value_objects::{dose_record_id::DoseRecordId, medication_id::MedicationId},
+};
+
+pub struct FakeDoseRecordRepository {
+    records: Mutex<Vec<DoseRecord>>,
+}
+
+impl FakeDoseRecordRepository {
+    pub fn new() -> Self {
+        Self {
+            records: Mutex::new(Vec::new()),
+        }
+    }
+
+    pub fn with(record: DoseRecord) -> Self {
+        Self {
+            records: Mutex::new(vec![record]),
+        }
+    }
+
+    pub fn saved_count(&self) -> usize {
+        self.records.lock().unwrap().len()
+    }
+}
+
+impl DoseRecordRepository for FakeDoseRecordRepository {
+    fn save(&self, record: &DoseRecord) -> Result<(), StorageError> {
+        let mut records = self.records.lock().unwrap();
+        if let Some(existing) = records.iter_mut().find(|r| r.id() == record.id()) {
+            *existing = record.clone();
+        } else {
+            records.push(record.clone());
+        }
+        Ok(())
+    }
+
+    fn find_by_id(&self, id: &DoseRecordId) -> Result<Option<DoseRecord>, StorageError> {
+        Ok(self
+            .records
+            .lock()
+            .unwrap()
+            .iter()
+            .find(|r| r.id() == id)
+            .cloned())
+    }
+
+    fn find_all_by_medication(
+        &self,
+        medication_id: &MedicationId,
+    ) -> Result<Vec<DoseRecord>, StorageError> {
+        Ok(self
+            .records
+            .lock()
+            .unwrap()
+            .iter()
+            .filter(|r| r.medication_id() == medication_id)
+            .cloned()
+            .collect())
+    }
+
+    fn delete(&self, _id: &DoseRecordId) -> Result<(), StorageError> {
+        Ok(())
+    }
+}
---
+++ src/application/ports/fakes/mod.rs
@@ -0,0 +1,9 @@
+mod fake_clock;
+mod fake_dose_record_repository;
+mod fake_medication_repository;
+mod fake_notification_port;
+
+pub use fake_clock::FakeClock;
+pub use fake_dose_record_repository::FakeDoseRecordRepository;
+pub use fake_medication_repository::FakeMedicationRepository;
+pub use fake_notification_port::FakeNotificationPort;
