a542f1cffbf8fe1cd06d492430154127c05f2501
Glauber Brennon <glauberbrennon@gmail.com>
2026-03-06 00:48:56 -0300

    style(infrastructure): improve architecture and test coverage

--- tests/infrastructure/integration_mark_taken_handler.rs
+++ tests/infrastructure/integration_mark_taken_handler.rs
@@ -1,8 +1,8 @@
 use bitpill::application::ports::fakes::FakeDoseRecordRepository;
 use bitpill::application::dtos::responses::MedicationDto;
-use bitpill::application::ports::inbound::mark_medication_taken_port::MarkMedicationTakenPort;
+use bitpill::application::ports::inbound::mark_medication_taken_port::MarkDoseTakenPort;
 use bitpill::application::ports::outbound::dose_record_repository_port::DoseRecordRepository;
-use bitpill::application::services::mark_medication_taken_service::MarkMedicationTakenService;
+use bitpill::application::services::mark_medication_taken_service::MarkDoseTakenService;
 use bitpill::presentation::tui::app::App;
 use bitpill::presentation::tui::app_services::AppServices;
 use bitpill::presentation::tui::handlers::medication_list_handler::MedicationListHandler;
@@ -16,7 +16,7 @@ fn medication_list_handler_saves_taken_dose_record_on_s() {
     let repo_trait: Arc<dyn DoseRecordRepository> = fake_repo.clone();
     let mut container = bitpill::infrastructure::container::Container::new();
     container.mark_medication_taken_service =
-        Arc::new(MarkMedicationTakenService::new(repo_trait)) as Arc<dyn MarkMedicationTakenPort>;
+        Arc::new(MarkDoseTakenService::new(repo_trait)) as Arc<dyn MarkDoseTakenPort>;
     let services = AppServices::from_container(&container);
 
     let mut app = App::new(services);
