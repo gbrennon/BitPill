d54abcbda407ac5a98f3a5bebcd25d757872c51d
Glauber Brennon <glauberbrennon@gmail.com>
2026-03-30 22:31:18 -0300

    ci(scripts): disable SC2034 for shell scripts

--- scripts/lib/context.sh
+++ scripts/lib/context.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export DRY_RUN=false
-export BUMP_TYPE="$1"
-export PRE_RELEASE="$2"
+# shellcheck disable=SC2034
+DRY_RUN=false
+BUMP_TYPE="$1"
+PRE_RELEASE="$2"
 
 parse_cli_arguments() {
   while [[ $# -gt 0 ]]; do
     case "$1" in
       --dry-run)
+        # shellcheck disable=SC2034
         DRY_RUN=true
         shift
         ;;
       --pre-release)
+        # shellcheck disable=SC2034
         PRE_RELEASE="$2"
         shift 2
         ;;
       patch|minor|major)
+        # shellcheck disable=SC2034
         BUMP_TYPE="$1"
         shift
         ;;
