f598c047d8c8587b76d87f0f8702b2f3e7aaba79
Glauber Brennon <glauberbrennon@gmail.com>
2026-03-31 16:13:58 -0300

    style(value_objects): formatting

--- src/domain/value_objects/mod.rs
+++ src/domain/value_objects/mod.rs
@@ -4,9 +4,9 @@ pub mod medication_frequency;
 pub mod medication_id;
 pub mod medication_name;
 pub mod medication_refill_id;
+pub mod navigation_mode;
 pub mod scheduled_time;
 pub mod taken_at;
-pub mod navigation_mode;
 
 pub use dosage::Dosage;
 pub use dose_record_id::DoseRecordId;
@@ -14,6 +14,6 @@ pub use medication_frequency::DoseFrequency;
 pub use medication_id::MedicationId;
 pub use medication_name::MedicationName;
 pub use medication_refill_id::MedicationRefillId;
+pub use navigation_mode::NavigationMode;
 pub use scheduled_time::ScheduledTime;
 pub use taken_at::TakenAt;
-pub use navigation_mode::NavigationMode;
