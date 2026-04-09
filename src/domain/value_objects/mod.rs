d801759b639e28e9d565e0f4f3de9394c6477a91
Glauber Brennon <glauberbrennon@gmail.com>
2026-04-09 04:01:54 -0300

    refactor(domain): update value_objects/mod.rs

--- src/domain/value_objects/mod.rs
+++ src/domain/value_objects/mod.rs
@@ -4,9 +4,11 @@ pub mod medication_frequency;
 pub mod medication_id;
 pub mod medication_name;
 pub mod medication_refill_id;
+pub mod medication_stock_id;
 pub mod navigation_mode;
 pub mod scheduled_time;
 pub mod scheduled_time_parser;
+pub mod stock_quantity;
 pub mod taken_at;
 
 pub use dosage::Dosage;
@@ -15,10 +17,12 @@ pub use medication_frequency::DoseFrequency;
 pub use medication_id::MedicationId;
 pub use medication_name::MedicationName;
 pub use medication_refill_id::MedicationRefillId;
+pub use medication_stock_id::MedicationStockId;
 pub use navigation_mode::NavigationMode;
 pub use scheduled_time::ScheduledTime;
 pub use scheduled_time_parser::{
     ParsedScheduledTimes, ScheduledTimeParseError, ScheduledTimeParseErrorKind,
     parse_scheduled_times,
 };
+pub use stock_quantity::StockQuantity;
 pub use taken_at::TakenAt;
