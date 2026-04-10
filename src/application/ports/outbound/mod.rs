9902f9e035aefa5edc3c47c4254b4a83a618927f
Glauber Brennon <glauberbrennon@gmail.com>
2026-04-10 02:03:50 -0300

    feat(application): define MedicationRefillRepositoryPort

--- src/application/ports/outbound/mod.rs
+++ src/application/ports/outbound/mod.rs
@@ -1,5 +1,6 @@
 pub mod clock_port;
 pub mod dose_record_repository_port;
+pub mod medication_refill_repository_port;
 pub mod medication_repository_port;
 pub mod medication_stock_repository_port;
 pub mod notification_port;
@@ -8,6 +9,7 @@ pub mod settings_repository_port;
 
 pub use clock_port::ClockPort;
 pub use dose_record_repository_port::DoseRecordRepository;
+pub use medication_refill_repository_port::MedicationRefillRepositoryPort;
 pub use medication_repository_port::MedicationRepository;
 pub use medication_stock_repository_port::MedicationStockRepositoryPort;
 pub use notification_port::NotificationPort;
